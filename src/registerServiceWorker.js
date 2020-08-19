11a79052c9011d13c48c6a8bd04e792b845cbaff
Luiz Freitas <luiz_dev@outlook.com>
2020-08-19 16:43:16 -0300

    [UPD] - serviceworker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -28,5 +28,8 @@ if (process.env.NODE_ENV === 'production') {
     error (error) {
       console.error('Error during service worker registration:', error)
     }
+  }).then(reg => {
+    console.log('Update')
+    reg.update()
   })
 }
